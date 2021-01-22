ef59a7b87f3e461ba41131ced7d929ff77e62d95
Amit Kadan <ak93.mtb@gmail.com>
2021-01-22 15:36:58 -0800

    cleaned up repo

--- sample.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --account=def-siamakx
-#SBATCH --gres=gpu:1
-#SBATCH --mem=4000
-#SBATCH --time=0-01:00
-#SBATCH --output=%N-%j.out
-module load miniconda3
-source activate pytorch
-python main.py -p -n 50000 -d 0 1
